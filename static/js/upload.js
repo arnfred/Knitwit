89d4577f21df4d07b450c9229fe607edd24cfaf9
jonas arnfred <jonas@ifany.org>
2014-04-19 22:58:14 +0800

    Reset progress bar on finish

--- static/js/upload.js
+++ static/js/upload.js
@@ -77,6 +77,7 @@ define(["lib/jquery", "js/capture", "text!templates/upload.html", "ractive", "ra
 			processData: false,
 			type: 'POST',
 			success: function(response){
+                progress(0);
 				show_preview($.parseJSON(response).path);
 			}
 		});
